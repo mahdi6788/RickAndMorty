380b8db611c127f910e4e3933d85c66855f586cb
mahdi <mjalalimech@gmail.com>
2024-10-15 17:35:57 +0400

    make local functions

--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -3,7 +3,7 @@ import { EyeIcon } from "@heroicons/react/16/solid";
 function CharacterList({ allCharacters }) {
   return (
     <div className="charecters-list">
-      {allCharacters.map (item => (
+      {allCharacters.map((item) => (
         <Character item={item} key={item.id} />
       ))}
     </div>
@@ -12,23 +12,34 @@ function CharacterList({ allCharacters }) {
 
 export default CharacterList;
 
-
 function Character({ item }) {
   return (
     <div className="list__item">
       <img src={item.image} alt={item.name} />
-      <h3 className="name">
-        <span>{item.gender === "Male" ? "👨" : "👩"} </span>
-        <span>{item.name}</span>
-      </h3>
-      <div className="list-item__info info">
-        <span className={`status ${item.status === "Dead" && "red"}`}></span>
-        <span> {item.status} </span>
-        <span>- {item.species}</span>
-      </div>
+      <CharacterName item={item} />
+      <CharacterInfo item={item} />
       <button className="icon red">
-        <EyeIcon/>
+        <EyeIcon />
       </button>
+    </div>
+  );
+}
+
+function CharacterName({ item }) {
+  return (
+    <h3 className="name">
+      <span>{item.gender === "Male" ? "👨" : "👩"} </span>
+      <span>{item.name}</span>
+    </h3>
+  );
+}
+
+function CharacterInfo({ item }) {
+  return (
+    <div className="list-item__info info">
+      <span className={`status ${item.status === "Dead" && "red"}`}></span>
+      <span> {item.status} </span>
+      <span>- {item.species}</span>
     </div>
   );
 }
