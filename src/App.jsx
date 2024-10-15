0d6fb4534a17dc4bd0f42cf70eaeb209e64a6210
mahdi <mjalalimech@gmail.com>
2024-10-15 14:50:40 +0400

    make characterDETAIL COMPONENT

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import "./App.css";
+import CharacterList from "./components/CharacterList";
+import CharecterDetail from "./components/CharecterDetail";
 import Navbar from "./components/Navbar";
+import {allCharacters} from "../data/data"
 
 function App(){
   return (
-    <div>
+    <div className="app">
       <Navbar />
+      <div className="main">
+        <CharacterList allCharacters={allCharacters} />
+        <CharecterDetail  />
+      </div>
+      
     </div>
   )
 }
