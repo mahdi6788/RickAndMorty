3e7fcef9c82a4194dbdcafc46941badf6cf57a3a
mahdi <mjalalimech@gmail.com>
2024-10-15 11:15:58 +0400

    edit navbar input classname

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,17 +2,15 @@ import { HeartIcon } from "@heroicons/react/24/outline";
 
 function Navbar() {
   return (
-    <div className="navbar">
+    <nav className="navbar">
       <div className="navbar__logo">Logo😍</div>
-
-      <input type="text" placeholder="search..." />
-      <p className="navbar__result">found X results</p>
-
-      <div className="heart">
+      <input className="text-field" type="text" placeholder="search..." />
+      <div className="navbar__result">found X results</div>
+      <button className="heart">
         <HeartIcon className="icon" />
         <span className="badge">4</span>
-      </div>
-    </div>
+      </button>
+    </nav>
   );
 }
 
