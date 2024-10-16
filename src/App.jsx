ae6b31ef9a510719f28d1d2d19be653aadf58215
mahdi <mjalalimech@gmail.com>
2024-10-16 12:56:55 +0400

    use two way to fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,37 @@ import CharacterList from "./components/CharacterList";
 import CharecterDetail from "./components/CharecterDetail";
 import Navbar, { SearchResult } from "./components/Navbar";
 import { allCharacters } from "../data/data";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   // we need to put charecters state into App compo because we need it for navbar as well.
-  const [characters, setCharacters] = useState(allCharacters);
+  const [characters, setCharacters] = useState([]);
+  // depending on the condition we decide to use useEffect or event handler function
+  // 1st approach: useEffect to fetch data
+  useEffect(() => {
+    fetch("https://rickandmortyapi.com/api/character")
+      .then((res) => res.json())
+      .then((data) => setCharacters(data.results));
+  }, []);
+
+  // // 2nd approach: event handler function (button) to fetch data
+  // function fetchCharacter() {
+  //   fetch("https://rickandmortyapi.com/api/character")
+  //     .then((res) => res.json())
+  //     .then((data) => setCharacters(data.results));
+  // }
+
+
   const numOfResult = characters.length;
   return (
     <div className="app">
+      {/* use component composition for navbar and use num of result as a child to prevent props drilling */}
       <Navbar>
-        <SearchResult numOfResult={numOfResult} />  // Component composition
+        <SearchResult numOfResult={numOfResult} /> 
       </Navbar>
+      {/* <button style={{ color: "red" }} onClick={fetchCharacter}>
+        Load Charecters
+      </button> */}
       <div className="main">
         <CharacterList characters={characters} />
         <CharecterDetail />
@@ -23,7 +43,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
