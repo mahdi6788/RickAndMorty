034df3a4550eda0bdb64a33e6bf6ec5116ae0ca9
mahdi <mjalalimech@gmail.com>
2024-10-16 18:16:41 +0400

    use axios instead of fetch and handle error by axios

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,21 @@ import CharecterDetail from "./components/CharecterDetail";
 import Navbar, { SearchResult } from "./components/Navbar";
 import { allCharacters } from "../data/data";
 import { useEffect, useState } from "react";
-import { Toaster } from "react-hot-toast";
+import toast, { Toaster } from "react-hot-toast";
+import axios from "axios";
 
 function App() {
-  // we need to put charecters state into App compo because we need it for navbar as well.
+  /// we need to put charecters state into App compo because we need it for navbar as well.
   const [characters, setCharacters] = useState([]);
-  // depending on the condition we decide to use useEffect or event handler function
-  // 1st approach: useEffect to fetch data
+  /// depending on the condition we decide to use useEffect or event handler function
+  /// 1st approach: useEffect to fetch data
   // useEffect(() => {
   //   fetch("https://rickandmortyapi.com/api/character")
   //     .then((res) => res.json())
   //     .then((data) => setCharacters(data.results));
   // }, []);
 
-  // // 2nd approach: event handler function (button) to fetch data
+  /// // 2nd approach: event handler function (button) to fetch data
   // function fetchCharacter() {
   //   fetch("https://rickandmortyapi.com/api/character")
   //     .then((res) => res.json())
@@ -26,20 +27,41 @@ function App() {
 
   // isLoading state and Loading component for showing message when user is waiting to fetch data from API
   const [isLoading, setIsLoading] = useState(false)
-  // use async await instead of then catch: 
-  // first declare function fetchData
+  /// use async await instead of then catch: 
+  /// first declare function fetchData
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     try {
+  //       setIsLoading(true)
+  //       const res = await fetch("https://rickandmortyapi.com/api/characters");
+  //       if (!res.ok) throw new Error("Error")
+  //       const data = await res.json();
+  //       setCharacters(data.results);
+  //       // setIsLoading(false)
+  //     } catch (error) {
+  //       // setIsLoading(false)
+  //       // console.log(error.message)
+  //       toast.error(error.message)   /// display error as a toast
+  //     } finally{
+  //       setIsLoading(false)
+  //     }
+  //   }
+  //   // then call that function
+  //   fetchData();
+  // }, []);
+
+  ///use axios instead of using fetch
+
   useEffect(() => {
     async function fetchData() {
       try {
         setIsLoading(true)
-        const res = await fetch("https://rickandmortyapi.com/api/characters");
-        if (!res.ok) throw new Error("Error")
-        const data = await res.json();
-        setCharacters(data.results);
-        // setIsLoading(false)
+        const res = await axios.get("https://rickandmortyapi.com/api/character");
+        // console.log(res.data.results)
+        setCharacters(res.data.results);
       } catch (error) {
-        // setIsLoading(false)
-        console.log(error.message)
+        // console.log(error.response.data.error)
+        toast.error(error.response.data.error)   /// display error as a toast
       } finally{
         setIsLoading(false)
       }
@@ -47,6 +69,8 @@ function App() {
     // then call that function
     fetchData();
   }, []);
+
+
 
 
   const numOfResult = characters.length;
